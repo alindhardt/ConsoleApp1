cda210ddf0636b91af63dcdaf891420926c376bb
Aputsiaq Lindhardt <aputsiaql@gmail.com>
2019-05-02 11:46:10 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Name = "Ole";
+            customer.Age = 25;
+            customer.CustomerID = 50001;
+            Console.WriteLine(customer);
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool Gender { get; set; }
+    }
+    class Customer : Person
+    {
+        public int CustomerID { get; set; }
+    }
+}
